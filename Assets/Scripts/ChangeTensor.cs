db30e669f19981f2224a4495854cb91094e4193a
damakoo <71433895+damakoo@users.noreply.github.com>
2023-07-04 12:53:51 +0900

    commti

---
+++ Assets/Scripts/ChangeTensor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTensor : MonoBehaviour
+{
+    float tensor = 10000;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.inertia = tensor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
